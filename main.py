5703501a077a0382d06c8387f52892b6e37de37f
Toneryy <144139291+Toneryy@users.noreply.github.com>
2023-09-19 23:49:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello world')
+import telebot
+
+TOKEN = '6058507940:AAEAb_bmD0lXXT_a742jKCXlHrYRfaGNsaI'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'menu'])
+def start(message):
+    bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.first_name}, я универсальный чат-бот для выдачи информации о погоде.\nДля выдачи информации о погоде в городе введите его название.')
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+
+
+
+
+bot.polling(none_stop=1)
